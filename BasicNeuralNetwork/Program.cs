c950a2ffb5b653c816cfb9ba45f20ef95cefd8f5
Enrico Rossini <enrico.rossini.uk@live.com>
2026-01-30 13:39:04 +0000

    Modelling Feed Forward

--- BasicNeuralNetwork/Program.cs
+++ BasicNeuralNetwork/Program.cs
@@ -9,6 +9,15 @@ nn.Dump("Neural Network Structure");
 var inputs = Matrix<double>.Build.DenseOfArray(new double[,] { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 } });
 inputs.Dump("Input Matrix");
 
+var outputs = Vector<double>.Build.DenseOfEnumerable(new List<double> { 1, 1, 1, 0 });
+outputs.Dump("Output Vector");
+
+// number of interactions for training
+int epochs = 100;
+
+// essentially for learning in neural networks
+double learningRate = 0.01;
+
 /// <summary>
 /// Creates the neural network.
 /// </summary>
@@ -35,5 +44,38 @@ NeuralNetwork ConnectNeuralNetwork(NeuralNetwork nn)
         return layer;
     }).ToList();
 
+    return nn;
+}
+
+#region Training the Neural Network
+
+/// <summary>
+/// Trains the specified neural network.
+/// </summary>
+/// <param name="nn">The neural network.</param>
+/// <param name="epochs">The epochs.</param>
+/// <param name="learningRate">The learning rate.</param>
+/// <param name="inputs">The inputs.</param>
+/// <param name="outputs">The outputs.</param>
+/// <returns>BasicNeuralNetwork.Models.NeuralNetwork.</returns>
+NeuralNetwork Train(NeuralNetwork nn, int epochs, double learningRate, Matrix<double> inputs, Vector<double> outputs)
+{
+    for (int epoch = 0; epoch < epochs; epoch++)
+    {
+        for (int trainRowIndex = 0; trainRowIndex < inputs.RowCount; trainRowIndex++)
+        {
+            //nn = FeedForward();
+            //nn = BackwardPass();
+            //nn = AdjustWeights();
+        }
+    }
+
+    return nn;
+}
+
+#endregion
+
+NeuralNetwork FeedForward(NeuralNetwork nn, Vector<double> inputRow)
+{
     return nn;
 }
